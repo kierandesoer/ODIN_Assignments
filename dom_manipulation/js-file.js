77217f191adc37f29d984ffb4fd54ae5acd6d259
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-06-17 15:45:34 +0100

    Merged staged changes

--- dom_manipulation/js-file.js
+++ dom_manipulation/js-file.js
@@ -1,40 +1,33 @@
-const container = document.querySelector("#container");
+const link = document.querySelector("a");
+        link.textContent = "Mozilla Developer Network";
+        link.href = "https://developer.mozilla.org";
 
-const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "This is the glorious text-content!";
+        const sect = document.querySelector("section");
+        const para = document.createElement("p");
+        // ####### Add inline styles ######## //
+        // para.style.color = "white";
+        // para.style.backgroundColor = "black";
+        // para.style.padding = "10px";
+        // para.style.width = "250px";
+        // para.style.textAlign = "center";
+        para.setAttribute("class", "highlight");
 
-const p_tag = document.createElement("p");
-p_tag.classList.add("p_tag");
-p_tag.textContent = "Hey I'm red!";
-p_tag.style.color = "red";
+        para.textContent = "We hope you enjoyed the ride.";
+        sect.appendChild(para);
 
-const h3_tag = document.createElement("h3");
-h3_tag.classList.add("h3_tag");
-h3_tag.textContent = "I'm a blue H3!";
-h3_tag.style.color = "blue";
+        const text = document.createTextNode(
+            " - the premier source for web development knowledge."
+        );
 
+        const linkPara = document.querySelector("p");
+        linkPara.appendChild(text);
+        sect.appendChild(linkPara);
+        //  remove node using node.removeChild()
+        // sect.removeChild(linkPara);
 
-
-const new_div = document.createElement("div");
-new_div.classList.add("new_div");
-new_div.setAttribute("style", "border: 1px solid; border-color: black; background: pink;");
-
-const h1_tag = document.createElement("h1");
-h1_tag.classList.add("h1_tag");
-h1_tag.textContent = "I'm in a div";
-
-const another_p_tag = document.createElement("p");
-another_p_tag.classList.add("another_p_tag");
-another_p_tag.textContent = "ME TOO!";
-
-new_div.appendChild(h1_tag);
-new_div.appendChild(another_p_tag);
+        // remove node by direct reference
+        // linkPara.remove();
 
 
-container.appendChild(content);
-container.appendChild(p_tag);
-container.appendChild(h3_tag);
-container.appendChild(new_div);
 
 
