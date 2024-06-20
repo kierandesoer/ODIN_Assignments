9ce87e0a3a4de729023ba3202a2d330d1cffa76e
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-06-20 13:20:45 +0100

    etch-a-sketch exercise completed (minus extra credits)

--- js-script.js
+++ js-script.js
@@ -1,28 +1,68 @@
+// set inital dimensions
+const canvasSize = 600;
+const gridSize = 16;
+let pixelDimension = canvasSize / gridSize + "px";
 
+// get and setup main container
 const container = document.querySelector("#container");
 container.classList.add("container");
+container.style.height = canvasSize + "px";
+container.style.width = canvasSize + "px";
 
-function createSketchArea(rows, columns, sketchWidth, sketchHeight) {
-    container.style.height = sketchHeight+"px";
-    container.style.width = sketchWidth+"px";
+
+// Add navbar
+const navBar = document.createElement("div");
+navBar.style.padding = "10px";
+const resizeBtn = document.createElement("button");
+resizeBtn.textContent = "Resize the grid";
+resizeBtn.addEventListener("click", () => {
+    let result = prompt("Enter the height/width dimension (must be less than 100)");
+    if (isNaN(result) || result > 100) {
+        alert("Must be a number i.e. 1,2,3... and less than or 100");
+        return;
+    } else {
+        createSketchArea(result);
+    }
+});
+navBar.appendChild(resizeBtn);
+container.appendChild(navBar);
+
+
+// Create container for the canvas
+const canvas = document.createElement("div");
+container.appendChild(canvas);
+
+// function to render the canvas
+function createSketchArea(gridSize) {
+
+    canvas.innerHTML = '';
+    let pixelDimension = canvasSize / gridSize + "px";
+    // set pixel_size in css
+    let pS = document.documentElement.style.setProperty('--pixel_size', pixelDimension);
 
     // Add row divs
-    for (i = 0; i < rows; i++) {
+    for (i = 0; i < gridSize; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
 
-        // Add column divs
-        for (n = 0; n < columns; n++) {
-            const column = document.createElement("div");
-            column.id = "col_" + n;
-            column.classList.add("column");
-            column.style.width = sketchWidth / columns +"px";
-            column.style.height = sketchHeight / rows +"px";
-            row.appendChild(column);
-        }
-        container.appendChild(row);
+        // Add pixel divs
+        for (n = 0; n < gridSize; n++) {
+            const pixel = document.createElement("div");
+            pixel.id = "pixel_" + i + "_" + n;
+            pixel.classList.add("pixel");
 
+            // Add hover event listener
+            pixel.addEventListener("mouseover", () => {
+                pixel.style.backgroundColor = "black";
+            }
+            );
+
+
+            row.appendChild(pixel);
+        }
+        canvas.appendChild(row);
     }
+    return;
 }
 
-createSketchArea(16,16,600,600);
+createSketchArea(gridSize);
